73e3fa84bfd42ce59471c0f57b1276c2ecb9717b
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2025-11-04 22:38:14 +0530

    knowing about promises

---
+++ advance_one/promises.js
@@ -0,0 +1,24 @@
+const promiseone=new Promise(function(resolve,reject){
+    // do an async task
+    setTimeout(function(){
+        console.log("Async task is complete");
+        resolve()
+    },1000)
+})
+
+promiseone.then(function(){
+    console.log(`promise consumed`);
+})
+
+
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log(`async task 2`);
+        resolve()
+        
+    },1000)
+}).then(function(){
+    console.log(`Async 2 resolved`);
+    
+})
