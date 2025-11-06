220c2d7116ba4e2e4c026f7f0063307ade787341
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2025-11-06 23:09:00 +0530

    knowing about more objects and classes

--- classes_&_oop/oop.js
+++ classes_&_oop/oop.js
@@ -1,7 +1,7 @@
 const user={
     username:"rudra",
     logincount:8,
-    Signedin:true,
+    isloggedin:true,
     getuserdetails:function(){
         console.log("got user details from database");
         // console.log(`username:${username}`);   
@@ -12,3 +12,29 @@ const user={
 
 console.log(user.username);
 console.log(user.getuserdetails());
+
+
+
+function User(username,logincount,isloggedin){
+    this.username=username;
+    this.logincount=logincount;
+    this.isloggedin=isloggedin;
+    this.greeting=function(){
+        console.log(`welcome ${this.username}`);
+    }
+    return this;
+}
+
+
+// const user1=User("rudra",15,true);
+// const user2=User("user two changed",150000,true);  
+
+//here the user two rewrite the value of user one
+// so we have to use new keyword to avoid this
+
+const user1=new User("rudra",15,true);
+const user2=new User("user two changed",150000,true);
+console.log(user1);
+console.log(user2);
+
+
